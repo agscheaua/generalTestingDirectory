37a9ac248345a386fa6aaefe60b58a9da1809eee
Alex <agscheaua@gmail.com>
2026-06-11 22:43:46 +0300

    Exercices 2 done form test practice

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,18 +1,8 @@
-
-
-test("capitalizeWordTEST", () => {
-  function capitalizeWord(word) {
+test("reverse a string", () => {
+  function reverseString(word) {
     const wordMadeAString = String(word);
-    const splitWord = wordMadeAString.split("");
-    const regexPatternAToZ = new RegExp("[A-Za-z]");
-    if (regexPatternAToZ.test(splitWord[0])) {
-      const capitalizeFirstChar = splitWord[0].toUpperCase();
-      splitWord[0] = capitalizeFirstChar;
-      const capitalizedWord = splitWord.join("");
-      return capitalizedWord;
-    } else {
-      throw new Error("The first character in the word is not a letter.");
-    };
-  }
-  expect(capitalizeWord("alex")).toBe("Alex");
-});  
+    const reverseWord = ( (wordMadeAString.split("")).reverse() ).join("");
+    return reverseWord; 
+  };
+  expect(reverseString("ale")).toBe("ela");
+});
