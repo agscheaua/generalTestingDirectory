c580c0169945fac05ac7a0436b9615e14b1e4beb
Alex <agscheaua@gmail.com>
2026-06-11 22:31:45 +0300

    i completed the first exercices from the test prac tice

--- src/index.js
+++ src/index.js
@@ -1,63 +1,14 @@
-import './styles.css';
-
-
-const button = document.querySelector("button");
-const img = document.querySelector("img");
-const gifTheme = document.getElementById("gifTheme");
-
-const gifsContainer = [];
-
-let topicName;
-function fetchGif(topic) {
-  for (let i = 0; i <= 8; i++) {
-    fetch("https://api.giphy.com/v1/gifs/translate?api_key=4LsvwfUDVa22JwbgNJrHzI7dPfY14rfT&s=" + String(topic))
-      .then(function(response) {
-        if (response.ok) {
-          return (response.json());
-        } else {
-          console.log("Response problems.");
-        };
-      })
-      .then(function(response) {
-          const imageReturned = response.data.images.original.url;
-          gifsContainer[i] = imageReturned;
-          img.src = gifsContainer[0];
-          console.log(gifsContainer);
-      });
-  };
-  topicName = topic;
-};
-
-let gifIndexInArray;
-function randomNr() {
-  let createRandomNr = Math.random();
-  let randomNrGiven = Math.floor((createRandomNr * 8));
-  if (randomNrGiven === 0) {
-    gifIndexInArray = randomNrGiven;
-    return;
-  } else {
-    let initalNr = Math.floor((createRandomNr * 8) + 1);
-    gifIndexInArray = initalNr;
-  };
-};
-
-function searchForNewGifs() {
-  button.addEventListener("click", (eve) => {
-    eve.preventDefault();
-    console.log(gifTheme.value);
-    if (gifTheme.value === "") {
-      console.log("Invalid search value.");
-    } else if (gifTheme.value === topicName) {
-      console.log("The search value was not changed.");
-      randomNr();
-      img.src = gifsContainer[gifIndexInArray];
-    } else {
-      fetchGif(gifTheme.value);
-      randomNr();
-    };
-  });
-};
-searchForNewGifs();
-
-
-
+    function capitalizeWord(word) {
+      const wordMadeAString = String(word);
+      const splitWord = wordMadeAString.split("");
+      const regexPatternAToZ = new RegExp("[A-Za-z]");
+      if (regexPatternAToZ.test(splitWord[0])) {
+        const capitalizeFirstChar = splitWord[0].toUpperCase();
+        splitWord[0] = capitalizeFirstChar;
+        const capitalizedWord = splitWord.join("");
+        return capitalizedWord;
+      } else {
+        throw new Error("The first character in the word is not a letter.");
+      };
+    }
+    console.log(capitalizeWord(""));
