13d51248ce0ef337fb1f64da1ea2c71dca3b5fc3
Alex <agscheaua@gmail.com>
2026-06-12 22:11:15 +0300

    exercices 3 from the test practice done

--- src/index.js
+++
@@ -1,6 +0,0 @@
-  function reverseString(word) {
-    const wordMadeAString = String(word);
-    const reverseWord = ( (wordMadeAString.split("")).reverse() ).join("");
-    return reverseWord; 
-  };
-  console.log(reverseString("121DAalex12"));
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,8 +1,22 @@
-test("reverse a string", () => {
-  function reverseString(word) {
-    const wordMadeAString = String(word);
-    const reverseWord = ( (wordMadeAString.split("")).reverse() ).join("");
-    return reverseWord; 
+ test("operating a value", () => {
+  const claculator = function() {
+    return {
+      add(a, b) {
+        return a + b;
+      },
+      substract(a, b) {
+        return a - b;
+      },
+      multiply(a, b) {
+        return a * b;
+      },
+      divide(a, b) {
+        if (b === 0) {
+          throw new Error("You can't divide a number with the 0 number.");
+        }
+        return a / b;
+      },
+    };
   };
-  expect(reverseString("ale")).toBe("ela");
+  expect(claculator().divide(1,)).toBe(0);
 });
